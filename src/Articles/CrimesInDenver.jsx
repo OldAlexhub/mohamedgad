197f3d32795d6724b3d241efd2af57cb9334de60
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-06-08 14:51:57 -0400

    Add files via upload

---
+++ src/Articles/CrimesInDenver.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CrimesInDenver = () => {
+  return (
+    <div>
+      <iframe
+        src="/Crimes-in-Denver.html"
+        style={{ width: "100%", height: "800px", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default CrimesInDenver;
