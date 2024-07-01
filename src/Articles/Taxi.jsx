978c3af29ce640dc43ef2c913e0e72d5221f8e7f
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-06-30 23:17:57 -0400

    Add files via upload

---
+++ src/Articles/Taxi.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Taxi = () => {
+  return (
+    <div>
+      <iframe
+        src="/taxi.html"
+        style={{ width: "100%", height: "800px", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Taxi;
