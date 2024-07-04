b77817889498e56eec101c2029585e44b4da79c5
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-07-04 17:18:21 -0400

    Add files via upload

---
+++ src/Articles/HRCaseStudy.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HRCaseStudy = () => {
+  return (
+    <div>
+      <iframe
+        src="/HRcasestudy.html"
+        style={{ width: "100%", height: "800px", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default HRCaseStudy;
