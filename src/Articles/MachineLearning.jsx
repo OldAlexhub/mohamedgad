bccbac848656ed78c561fad28ed75f26d3153c26
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-07-07 23:14:44 -0400

    Add files via upload

---
+++ src/Articles/MachineLearning.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MachineLearning = () => {
+  return (
+    <div>
+      <iframe
+        src="https://mohamedgad.shinyapps.io/Sales/"
+        style={{ width: "1500px", height: "800px", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default MachineLearning;
