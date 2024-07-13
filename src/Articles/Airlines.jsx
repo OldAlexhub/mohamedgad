26080b81c6e7947b3de69c4252ae68ae959d0ea9
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-07-12 20:47:36 -0400

    Add files via upload

---
+++ src/Articles/Airlines.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Airlines = () => {
+  return (
+    <div>
+      <iframe
+        src="/invesitco.pdf"
+        style={{ width: "100%", height: "800px", border: "none" }}
+        title="invistcoairlines"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Airlines;
