9ab94858f2b745dea4839ba3a38561d796a5cab4
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-08-14 22:15:40 -0400

    Add files via upload

---
+++ src/Articles/Sentiment.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Sentiment = () => {
+  return (
+    <div>
+      <iframe
+        src="https://mohamedgad.shinyapps.io/AISentimentmachine/"
+        style={{ width: "1500px", height: "800px", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Sentiment;
