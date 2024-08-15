2a675bd2b5ae751735be0093306e2230b51b3061
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-08-14 22:48:39 -0400

    Update Sentiment.jsx

--- src/Articles/Sentiment.jsx
+++ src/Articles/Sentiment.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Sentiment = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100vw", margin: 0, padding: 0 }}>
+    <div style={{ width: "100%", height: "110vh", overflow: "hidden" }}>
       <iframe
         src="https://mohamedgad.shinyapps.io/AISentimentmachine/"
-        style={{ width: "100%", height: "100%", border: "none", margin: 0, padding: 0 }}
+        style={{ width: "100%", height: "100%", border: "none" }}
         title="Dashboard"
       ></iframe>
     </div>
