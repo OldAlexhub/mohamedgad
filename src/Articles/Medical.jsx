f99d392a51b532dc397958e05f9d4cf9be4aab91
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-08-15 22:56:20 -0400

    Add files via upload

---
+++ src/Articles/Medical.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Medical = () => {
+  return (
+    <div style={{ width: "100%", height: "110vh", overflow: "hidden" }}>
+      <iframe
+        src="https://mohamedgad.shinyapps.io/Medical/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Medical;
