67c68a07231bbbc3087b00ada730ee609e57e4c0
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-08-18 18:46:28 -0400

    Add files via upload

---
+++ src/Articles/HRHiring.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HRHiring = () => {
+  return (
+    <div style={{ width: "100%", height: "110vh", overflow: "hidden" }}>
+      <iframe
+        src="https://mohamedgad.shinyapps.io/AIHR/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default HRHiring;
