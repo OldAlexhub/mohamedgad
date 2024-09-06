669cfbbb0abc253a5ba75269184911d0340510bb
Mohamed Gad <140672061+OldAlexhub@users.noreply.github.com>
2024-09-05 23:14:56 -0400

    Add files via upload

---
+++ src/Articles/AIcomplaint.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AIcomplaint = () => {
+  return (
+    <div>
+      <iframe
+        src="https://complaint-svn9.onrender.com/"
+        style={{ width: "100%", height: "800px", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default AIcomplaint;
