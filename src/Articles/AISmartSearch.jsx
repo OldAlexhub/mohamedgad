963707b33e6cedb313037d49d1e6f249c4f4cb62
Mohamed Gad <140672061+OldAlexhub@users.noreply.github.com>
2024-09-16 22:33:51 -0400

    Add files via upload

---
+++ src/Articles/AISmartSearch.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AISmartSearch = () => {
+  return (
+    <div style={{ width: "100%", height: "110vh", overflow: "hidden" }}>
+      <iframe
+        src="https://smartsearch-kh1y.onrender.com/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Dashboard"
+      ></iframe>
+    </div>
+  );
+};
+
+export default AISmartSearch;
